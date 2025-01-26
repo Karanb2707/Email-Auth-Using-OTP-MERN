94a470bc327aad9a16616e80aeea29e1dd7e8322
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-01-26 23:37:01 +0530

    singup api continue

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth', router);
 
+app.use(express.json()); //allow us to parse incoming requests: req.body
+
 app.listen(PORT , () => {
     connectDB();
     console.log('Server is running on port',PORT);
