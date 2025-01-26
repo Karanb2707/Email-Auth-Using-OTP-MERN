01838fbe980d1af5fc0a6f5dee111cf4578b8f44
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-01-26 13:11:59 +0530

    MongoDB Connection Done

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './db/connectDB.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -7,6 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000, () => {
+    connectDB();
     console.log('Server is running on port 3000');
 });
 
