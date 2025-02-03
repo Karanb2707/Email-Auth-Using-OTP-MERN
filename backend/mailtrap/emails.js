0364037b5a141703ab6c65bfc52725cfa6dae6a1
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-02-03 21:35:29 +0530

    Making Sending Email Config

---
+++ backend/mailtrap/emails.js
@@ -0,0 +1,21 @@
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js"
+import { mailtrapClient } from "./mailtrap.config.js"
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+    const recipient = [{ email }]
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Verify Your Email",
+            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}",verificationToken),
+            category: "Email Verification"
+        })
+
+        console.log("Email Sent Successfully!!", response);
+    } catch (error) {
+        console.log('Error sending verification email:', error);
+        throw new Error(`Error sending verificaton email: ${error}`);
+    }
+}
