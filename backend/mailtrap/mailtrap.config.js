196611048097c4d77e05e20cde7bf889fe0e2c7d
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-02-03 21:05:34 +0530

    Some Changes In mailtarp file

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -3,28 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const TOKEN = process.env.MAILTRAP_TOKEN;
-const ENDPOINT = process.env.MAILTRAP_ENDPOINT;
+export const mailtrapClient = new MailtrapClient({
+  endpoint: process.env.MAILTRAP_ENDPOINT,
+  token: process.env.MAILTRAP_TOKEN,
+});
 
-const client = new MailtrapClient({endpoint: ENDPOINT, token: TOKEN,});
-
-const sender = {
+export const sender = {
   email: "hello@demomailtrap.com",
   name: "Karan Bolake ",
 };
-
-const recipients = [
-  {
-    email: "karanbolake57@gmail.com",
-  }
-];
-
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    html: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
