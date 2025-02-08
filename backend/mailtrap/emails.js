16e4ae4573eed7738869760c99917a8fced6af68
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-02-08 20:46:28 +0530

    Verify and sending welcome mail done

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -18,4 +18,25 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         console.log('Error sending verification email:', error);
         throw new Error(`Error sending verificaton email: ${error}`);
     }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+    const recipient = [{ email }]
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid: "366b6d27-5cba-4d13-a437-87573d5719e3",
+            template_variables: {
+                "name": name,
+                "company_info_name": "KK Fintech Solutions"
+            }
+        });
+
+        console.log("Welcome email sent successfully", response);
+    } catch (error) {
+        console.error(`Error sending welcome email`, error);
+        throw new Error(`Error sending welcome email ${error}`);
+    }
 }
